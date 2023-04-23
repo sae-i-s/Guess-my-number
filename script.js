1af796bd710e07590668375c3cc4cafeb4ab0f69
Sae Ishima-Sell <96847968+sae-i-s@users.noreply.github.com>
2023-04-23 21:01:33 +0200

    Update script.js
    fixed typo

--- script.js
+++ script.js
@@ -45,8 +45,8 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess !== secretNumber) {
     if (score > 1) {
       //document.querySelector('.message').textContent =
-      //guess > secretNumber ? '📈 Too high!' : '📉 Too row!';
-      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too row!');
+      //guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
